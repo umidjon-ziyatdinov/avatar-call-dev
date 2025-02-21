f16417847e9f52389741476f1a80770f378c06c7
umidjon <sokcho69@workershigh.com>
2025-02-22 00:29:03 +0900

    auth .config updated

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -12,33 +12,28 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const userRole = auth?.user?.role; // Assuming the role is stored in auth.user.role
-
       const isOnChat = nextUrl.pathname.startsWith('/');
       const isOnRegister = nextUrl.pathname.startsWith('/register');
       const isOnLogin = nextUrl.pathname.startsWith('/login');
 
-      if (isLoggedIn) {
-        if (isOnLogin || isOnRegister) {
-          if (userRole === 'admin') {
-            return Response.redirect(new URL('/dashboard', nextUrl as unknown as URL));
-          } else if (userRole === 'moderator') {
-            return Response.redirect(new URL('/', nextUrl as unknown as URL));
-          }
-        }
-
-        if (isOnChat && userRole === 'admin') {
-          return Response.redirect(new URL('/dashboard', nextUrl as unknown as URL));
-        }
-
-        return true;
+      if (isLoggedIn && (isOnLogin || isOnRegister)) {
+        return Response.redirect(new URL('/', nextUrl as unknown as URL));
       }
 
       if (isOnRegister || isOnLogin) {
-        return true; // Allow access to register and login pages
+        return true; // Always allow access to register and login pages
       }
 
-      return Response.redirect(new URL('/login', nextUrl as unknown as URL)); // Redirect unauthenticated users
+      if (isOnChat) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      }
+
+      if (isLoggedIn) {
+        return Response.redirect(new URL('/', nextUrl as unknown as URL));
+      }
+
+      return true;
     },
   },
 } satisfies NextAuthConfig;
